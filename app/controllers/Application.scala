6ebf468561cf3aa2e88d6724c71fb96d5ebb5ab0
Chinthiti Wisetsombat <chinnonae@gmail.com>
2016-11-20 21:53:35 +0700

    move some Application's methods out

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,11 +1,13 @@
 package controllers
 
+import play.api.mvc._
+import play.api.mvc.Action
+
+import scala.concurrent.ExecutionContext.Implicits.global
 import javax.inject.Inject
 
 import models.DiseaseRepo
-import play.api.mvc._
-import play.api.mvc.Action
-import scala.concurrent.ExecutionContext.Implicits.global
+
 
 
 class Application @Inject() (diseaseRepo: DiseaseRepo) extends Controller {
@@ -16,21 +18,10 @@ class Application @Inject() (diseaseRepo: DiseaseRepo) extends Controller {
     }
   }
 
-  def diseasesPage = Action.async { implicit request =>
-    diseaseRepo.all.map {
-      diseases => Ok(views.html.diseases(diseases))
-    }
-  }
 
-  def newIssuePage = Action {
-    Ok(views.html.new_issue())
-  }
 
-  def diseasesDetailPage(id: Int) = Action.async { implicit request =>
-    for {
-      Some(disease) <- diseaseRepo.findById(id)
-    } yield Ok(views.html.detail(disease))
 
-  }
+
+
 
 }
